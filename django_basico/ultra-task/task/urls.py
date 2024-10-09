97136e4fd336d0fc1717b38aa0b6eba8db2ba9cf
Angel-L-G <angelchiquitopons@gmail.com>
2024-10-09 20:27:59 +0100

    feat: some more changes on ultra-task;

--- django_basico/ultra-task/task/urls.py
+++ django_basico/ultra-task/task/urls.py
@@ -6,5 +6,7 @@ app_name = 'task'  # This is your app name in the URL
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('task/done/', views.done, name='done'),
+    path('task/undone/', views.undone, name='undone'),
     path('task/<task_slug>/', views.task_detail, name='task_detail'),
 ]
--- django_basico/ultra-task/task/views.py
+++ django_basico/ultra-task/task/views.py
@@ -18,3 +18,17 @@ def task_detail(request, task_slug):
     task = Task.objects.get(slug=task_slug)
     time_left = (task.due_date - date.today()).days
     return render(request, 'task/task_detail.html', {'task': task, 'time_left': time_left})
+
+
+def done(request):
+    all_tasks = Task.objects.all()
+    finished = [task for task in all_tasks if task.completed]
+    status = 'Done'
+    return render(request, 'task/task_list.html', {'tasks': finished, 'status': status})
+
+
+def undone(request):
+    all_tasks = Task.objects.all()
+    not_finished = [task for task in all_tasks if not task.completed]
+    status = 'Done'
+    return render(request, 'task/task_list.html', {'tasks': not_finished, 'status': status})
